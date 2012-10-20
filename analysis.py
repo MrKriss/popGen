ce6a9bc18a6c3e5f23a8912308c4e7471546be58
Chris Musselle <chris.j.musselle@gmail.com>
2012-10-20 21:40:46 +0100

    Made analysis script more generic

--- analysis.py
+++ analysis.py
@@ -8,65 +8,57 @@ from Bio import SeqIO
 import numpy as np
 import time
 import os
-import cPickle as pkl
-import gzip
 
+def getPropN_meanPhred(inFiles = None, fileType = '', dataPath = ''):
+    ''' Return the proportion of Ns and the mean Phred scores of the files given'''
 
-def countRecords(fileName):
-    ''' Counts number of records in file'''
+    if dataPath:
+        os.chdir(dataPath)
+  
+    # Handle multiple types of input
+    if not inFiles:
+        # Fetch files by file types
+        assert fileType, 'No files listed and No file type specified.'
+        import glob
+        inFiles = glob.glob(fileType)
+    elif type(inFiles) == str:
+        # Convert to list
+        inFiles = [inFiles]
 
-    idxFileName = '.'.join(fileName.split('.')[:-2]) + '.idx'
-    S = SeqIO.index_db(idxFileName, format='fastq')
-
-    return   len(S)
-
-if __name__ == '__main__':
-
-#    dataLoacation = '/Users/chris/Datasets/gazellesZebras/'
-    dataLoacation = '/space/musselle/datasets/gazellesAndZebras/'
-    os.chdir(dataLoacation)
-
-#    filesLane8_1 = ["lane8/lane8_NoIndex_L008_R1_00%i.fastq.gz" % (i+1) for i in range(9)]
-#    filesLane8_2 = ["lane8/lane8_NoIndex_L008_R1_0%i.fastq.gz" % (i+10) for i in range(1)]
-    filesLane6_1 = ["lane6/lane6_NoIndex_L006_R1_00%i.fastq.gz" % (i+1) for i in range(9)]
-    filesLane6_2 = ["lane6/lane6_NoIndex_L006_R1_0%i.fastq.gz" % (i+10) for i in range(2)]
-#    fileNames = filesLane8_1 + filesLane8_2 # + filesLane6_1 + filesLane6_2
-    fileNames = filesLane6_1 + filesLane6_2
-    numFiles = len(fileNames)
-
-
-    toc = time.time()
-    recordCounts = map(countRecords, fileNames)
-    tek = time.time() - toc
-
-    print 'Finished counting records for {0} files in {1}'.format(numFiles,
-                time.strftime('%H:%M:%S', time.gmtime(tek)))
-
-    # Construct Data structures
+    # Define vars and outputs
+    numFiles = len(inFiles)
     outList = [0] * numFiles
-    for i in range(numFiles):
-        outList[i] = {'propN':  np.zeros(recordCounts[i])  ,
-                      'meanPhred': np.zeros(recordCounts[i]) }
+    recordCounts = [0] * numFiles
 
-    # Fill with stats
-    tik = time.time()
+    # Find the number of records in each file.
+    toc = time.time()
     cum_t = 0
-    for idx, f in enumerate(fileNames):
-        # Change Files to .bgzf extenstion
-        bgzfFileName = '.'.join(f.split('.')[:-1]) + '.bgzf'
-        with gzip.open(bgzfFileName, 'r') as handle:
-            S = SeqIO.parse(handle, 'fastq')
-            for c, seqR in enumerate(S):
-                # Proportion of Ns
-                outList[idx]['propN'][c] = float(seqR.seq.count('N')) / len(seqR)
-                # Mean Phred Score
-                outList[idx]['meanPhred'][c] = np.array(seqR.letter_annotations['phred_quality']).mean()
-
-        loop_t = time.time() - tik - cum_t
+    for fileNo, fileName in enumerate(inFiles):
+        # Check its a .idx file extention or that one exists with it.
+        if fileName.split('.')[-1] == '.idx':
+            idxFileName = fileName
+        else:
+            assert os.path.isfile('.'.join(fileName.split('.')[:-2]) + '.idx'),  'Index file not found for {0}'.format(fileName)
+            idxFileName = '.'.join(fileName.split('.')[:-2]) + '.idx'
+    
+        # Load file 
+        S = SeqIO.index_db(idxFileName, format='fastq')
+        recordCounts[fileNo] = len(S)
+        
+        outList[fileNo] = {'propN'  :  np.zeros(recordCounts[fileNo]),
+                        'meanPhred' : np.zeros(recordCounts[fileNo])  }
+        
+        # Cycle through all records 
+        for num, seqRec in enumerate(S.itervalues()):
+            # Proportion of Ns
+            outList[fileNo]['propN'][num] = float(seqRec.seq.count('N')) / len(seqRec)
+            # Mean Phred Score
+            outList[fileNo]['meanPhred'][num] = np.array(seqRec.letter_annotations['phred_quality']).mean()
+        
+        loop_t = time.time() - toc - cum_t
         cum_t += loop_t
-        print 'Finished {0} after {1}'.format(bgzfFileName, time.strftime('%H:%M:%S', time.gmtime(loop_t)))
+        print 'Finished {0} after {1}'.format(fileName, time.strftime('%H:%M:%S', time.gmtime(loop_t)))
 
-    # Construct and fill total Records stat object
     totalRecords = np.sum(recordCounts)
     totalStats = {'propN':  np.zeros(totalRecords)  ,
                   'meanPhred': np.zeros(totalRecords) }
@@ -78,11 +70,25 @@ if __name__ == '__main__':
         totalStats['meanPhred'][start_idx:end_idx] = statObj['meanPhred']
         start_idx += recordCounts[file_idx]
 
-    # Pickle data dictionary
+    # Saving data
     print 'Saving Data......'
-    np.save('lane6_propN', totalStats['propN'])
-    np.save('lane6_meanPhred', totalStats['meanPhred'])
+    parent_dir  = os.getcwd().split('/')[-1]
+    np.save(parent_dir + 'propN', totalStats['propN'])
+    np.save(parent_dir + 'lane6_meanPhred', totalStats['meanPhred'])
     print 'DONE! Data saved to {0}'.format(os.getcwd())
 
     total_t = time.time() - toc
     print 'Processed {0} files in {1}'.format(numFiles, time.strftime('%H:%M:%S', time.gmtime(total_t)))
+
+    return totalStats
+
+
+
+if __name__ == '__main__':
+
+#    dataLoacation = '/Users/chris/Datasets/gazellesZebras/'
+    dataLoacation = '/space/musselle/datasets/gazellesAndZebras/lane6'
+
+    getPropN_meanPhred(fileType = '*.idx', dataPath = dataLoacation)
+    
+ 
