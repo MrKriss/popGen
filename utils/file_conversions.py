e32d25118cc86f500e8e0f45b99ccdb6699623c9
Chris Musselle <chris.j.musselle@gmail.com>
2012-12-20 13:23:12 +0000

    Retest version 2, then version 1 of process MIDtags

--- utils/file_conversions.py
+++ utils/file_conversions.py
@@ -392,7 +392,7 @@ if __name__ == '__main__':
 #                   barcode_path=barpath)
     
 #===============================================================================
-# Run Process MID Tags on all data 
+# Test Process MID Tags 2
 #===============================================================================
     
     import glob
@@ -408,7 +408,80 @@ if __name__ == '__main__':
 #    raw_files = glob.glob('*[0-9].fastq.bgzf')
 #    raw_files.sort()
     
-    raw_files = ['lane6_NoIndex_L006_R1_001.fastq.bgzf']
+    raw_files = ['lane6_NoIndex_L006_R1_003.fastq.bgzf']
+    
+    outdir = 'L6_phredprop_filtered'
+
+    # Update names and path
+    filtered_files = []
+    for name in raw_files:
+        temp = name.split('.')
+        temp[0] = temp[0] + '-pass'
+        temp = '.'.join(temp) 
+        filtered_files.append(temp)
+    filtered_datapath = datapath + '/' + outdir
+    
+    cleaned_file_postfix = '-clean' 
+    cleaned_outdir = 'cleaned_data'
+    barcode_pattern = '*[' + LANE + '].txt'
+    
+    process_MIDtag2(infiles=filtered_files, barcodes=barcode_pattern,
+               barcode_pattern=True, datapath=filtered_datapath, 
+               barcode_path=barpath, outfile_postfix=cleaned_file_postfix, 
+               outdir=cleaned_outdir)
+    
+    # Update names and path
+    cleaned_files = []
+    for name in filtered_files:
+        
+        temp = name.split('.')
+                
+        # Replace postfix
+        if name.endswith('fastq.bgzf'):
+            temp[0] = temp[0] + cleaned_file_postfix
+            temp = '.'.join(temp[:-1])            
+        else:
+            temp = '.'.join(temp) 
+        cleaned_files.append(temp) 
+        
+    cleaned_datapath = filtered_datapath + '/' + cleaned_outdir
+    
+    #===============================================================================
+    # Cluster Data 
+    #===============================================================================
+    allreads_file = 'lane' + LANE + 'allreads-clean.fasta'
+    reads2fasta(infiles=cleaned_files, datapath=cleaned_datapath, outfile=allreads_file)
+    
+    # Variables 
+    c_thresh = 0.9
+    n_filter = 8
+    
+    clustered_file = 'lane' + LANE + 'clustered_reads'
+    cluster_cdhit(infile=allreads_file, outfile=clustered_file,
+                  c_thresh=c_thresh, n_filter=n_filter)
+    
+    # Display Summary
+    summary(clustered_file)
+    
+    
+#===============================================================================
+# Test Process MID Tags 1 
+#===============================================================================
+    
+    import glob
+    from cluster import cluster_cdhit, summary
+    
+    LANE = '6'
+    starting_dir = os.getcwd()
+
+    # Set paths and file patterns 
+    datapath = '/space/musselle/datasets/gazellesAndZebras/lane' + LANE
+    barpath = '/space/musselle/datasets/gazellesAndZebras/barcodes'
+    os.chdir(datapath)
+#    raw_files = glob.glob('*[0-9].fastq.bgzf')
+#    raw_files.sort()
+    
+    raw_files = ['lane6_NoIndex_L006_R1_002.fastq.bgzf']
     
     outdir = 'L6_phredprop_filtered'
 
@@ -455,6 +528,7 @@ if __name__ == '__main__':
     
     # Display Summary
     summary(clustered_file)
+    
     
 
 
