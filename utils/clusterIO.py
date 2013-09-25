2ac44430d7887c53149ec8e0b5ce4d9ad5487707
Chris Musselle <chris.j.musselle@gmail.com>
2013-09-25 13:24:20 +0100

    Testing Genotype and align functions

--- utils/clusterIO.py
+++ utils/clusterIO.py
@@ -171,8 +171,10 @@ class ClusterObj(object):
         temp_file.flush()
         temp_file.close()
 
+        temp_filepath = os.path.join(os.path.curdir, temp_file.name)
+
         # Align with MUSCLE
-        cline = MuscleCommandline(os.path.expanduser(muscle_exec_path), input=temp_file.name)
+        cline = MuscleCommandline(os.path.expanduser(muscle_exec_path), input=temp_filepath)
 
         child = subprocess.Popen(str(cline), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
