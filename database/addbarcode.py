82bcce0f32b3e24eb85ae2ad02b63e2fc6044f4b
Chris Musselle <chris.j.musselle@gmail.com>
2013-10-04 15:13:05 +0100

    Added clf to add dummy bacodes to RADseq files

---
+++ database/addbarcode.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#
+
+# import modules used here -- sys is a very standard one
+import sys, argparse, logging
+from Bio import SeqIO
+import random
+
+# Gather code in a main() function
+def main(args, loglevel):
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
+
+    num_files = len(args.inputs)
+    bases = 'ATGC'
+    barcode_dict ={}
+
+    # For each file, generate a barcode, append it to all sequence descriptions in the file
+    for i in range(num_files):
+
+        filename = args.inputs[i]
+
+        while True:
+            # Generate barcode
+            tag = ''.join(random.choice(bases) for x in range(6))
+            # check its different from all others
+            if not tag in barcode_dict:
+                barcode_dict[tag] = filename
+                break
+
+        # Append to all sequences in the description
+        seqgen = SeqIO.parse(open(filename), 'fastq')
+
+        outfile = open(filename + '.temp', 'wb')
+
+        for seq in seqgen:
+            seq.description += tag
+            SeqIO.write(seq, outfile, 'fastq')
+
+        logging.info('Finished processing {}. Written to {}'.format(filename, outfile.name))
+
+
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Takes a list of preprocessed files from Floragenics and inserts a dummy barcode on the front so they can be "
+                    "rerun through a preprocessing step using STACKS.",)
+
+    parser.add_argument(
+        "inputs",
+        help="List of inputs files to process",
+        nargs='+')
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="increase output verbosity",
+        action="store_true")
+    args = parser.parse_args()
+
+    # Setup logging
+    if args.verbose:
+        loglevel = logging.DEBUG
+    else:
+        loglevel = logging.INFO
+
+    main(args, loglevel)
