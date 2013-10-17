af8b5a9b1961d465019d7286799f19517602065b
Chris Musselle <chris.j.musselle@gmail.com>
2013-10-17 16:03:53 +0100

    Changed scripts to separate out concatonation and execution of ustacks due to memory constraints

--- clf/make_superparent_fastq.py
+++ clf/make_superparent_fastq.py
@@ -55,27 +55,7 @@ def main(args, loglevel):
         print cat_cmd
         with open(merged_filepath, 'wb') as merged_file:
             subprocess.check_call(cat_cmd.split(), stdout=merged_file, stderr=subprocess.STDOUT)
-        logging.info('Superparent created for {} and written to\n{}\n'.format(subpop, args.sup_parent_path))
-
-        # Run ustacks on the Superparent
-        # ustacks -t file_type -f file_path [-d] [-r] [-o path] [-i id] [-m min_cov] [-M max_dist] [-p num_threads]
-        # [-R] [-H] [-h]
-
-        ustacks_cmd = 'ustacks -t fastq -f {} -o {} -i {} -p {} -r -d'.format(merged_filepath,
-                                                                              args.stack_path,
-                                                                              i,
-                                                                              args.processors)
-        # Add extra options if present
-        if args.mindepth:
-            ustacks_cmd += '-m {}'.format(args.mindepth)
-        if args.maxdist_between_stacks:
-            ustacks_cmd += '-M {}'.format(args.maxdist_between_stacks)
-        if args.maxdist_second2primary:
-            ustacks_cmd += '-N {}'.format(args.maxdist_second2primary)
-
-        logging.debug("About to run ustacks with following comandline arguments:\n{}\n".format(str(ustacks_cmd.split())))
-        subprocess.check_call(ustacks_cmd.split())
-        logging.info("Finished processing {} of {} subpopulations.".format(i, len(args.subpops)))
+        logging.info('Superparent created for {} and written to\n{}\n'.format(subpop, merged_filepath))
 
 # Standard boilerplate to call the main() function to begin
 # the program.
@@ -105,31 +85,6 @@ if __name__ == '__main__':
         "-u", "--sup_parent_path",
         required=True,
         help="Location to write superparent output")
-
-    parser.add_argument(
-        "-t", "--stack_path",
-        required=True,
-        help="Location to write ustacks output")
-
-    parser.add_argument(
-        "-p", "--processors",
-        help="Number of processors to run ustacks with.",
-        default=1)
-
-    parser.add_argument(
-        "-m", "--mindepth",
-        help="Minimum depth of coverage required to create a stack in ustacks (default 2).",
-        default=None)
-
-    parser.add_argument(
-        "-M", "--maxdist_between_stacks",
-        help="Maximum distance (in nucleotides) allowed between stacks in ustacks (default 2).",
-        default=None)
-
-    parser.add_argument(
-        "-N", "--maxdist_second2primary",
-        help="Maximum distance allowed to align secondary reads to primary stacks in ustacks (default: M + 2).",
-        default=None)
 
     parser.add_argument(
         "-v", "--verbose",
